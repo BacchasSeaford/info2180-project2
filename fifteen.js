1da1fa1649b765fc9001ac56878a724285419f68
BacchasSeaford <richybacchas0911@gmail.com>
2016-10-30 18:12:52 -0500

    puzzle up to date

--- fifteen.js
+++ fifteen.js
@@ -1,19 +1,17 @@
+var shuffule = 0; 
 window.onload = function (){
-	SetbgImg();
 	setpos();
 	movepiece();
 }
 
-function SetbgImg(){
-	var x = document.getElementById("puzzlearea").style.background= "url('background.jpg')";
-}
 var klam = [];
+var badboy=[];
 function setpos(){ //Set position of puzzle in solved state 
 	var klam = $("#puzzlearea div");
 	$("#puzzlearea div").addClass("puzzlepiece");
 	var x = 0;
 	var q = 0;
-	for (var i = 0; i < 15; i++){
+	for (var i =0; i < 15; i++){
 		if (i < 4 ){
 				x = 0;
 			}
@@ -51,6 +49,7 @@ function movepiece(){ // set animation to puzzle pieces
 			emptyLeft=selectedTileLeft;
 			this.style.top = tempT +'px';
 			this.style.left = tempL +'px';
+			endofGame(); // check if puzzle is solve
 			}
 		});	
 			klam[i].addEventListener('mouseover', function(){ // set hover property 
@@ -68,9 +67,11 @@ function movepiece(){ // set animation to puzzle pieces
 	}); 
 }
 		document.getElementById("shufflebutton").addEventListener("click", function shuffle(){ // shuffle puzzle pieces in a random order
-			var x = Math.floor(Math.random() * 25);
+			$("body").css("background-color", "white");
+			var x = Math.floor(Math.random() * 250);
+			var t = 1;
 				for (var i = 0; i < x; i++){
-					for (var y = 14; y > 0; y--){
+					for (var y = 0; y < 15; y++){
 						var top = parseInt(klam[y].style.top);
 						var left = parseInt(klam[y].style.left);
 						var tempT = emptyTop;
@@ -83,13 +84,38 @@ function movepiece(){ // set animation to puzzle pieces
 						}
 					}
 				
-				}				
-			return x;});	
+				}
+			});	
 }
-function endofGame(){
-	var end = shuffle();
-	if (end > 0){
-		
+function endofGame(){ // determine if the puzzle is solved and display 
+	var klam = $("#puzzlearea div");
+	var x = 0;
+	var q = 0;
+	var won = 1;
+	for (var i = 0; i < 15; i++){
+				if (i < 4 ){
+				x = 0;
+			}
+				else if(i > 3 && i < 8){
+					x = 1;
+				}
+					else if(i > 7 && i < 12){
+								x = 2;
+					}
+						else	{
+									x = 3;
+								}
+		if (klam[i].style.top == 100 * x + "px" && klam[i].style.left == 100 * q + "px"){
+			won++;
+		}
+			q++;
+			if (q == 4){
+				q = 0;
+			}
+	} if (won == 16){
+			$("body").css("background-color", "blue");
 	}
-}
+	else
+	{$("body").css("background-color", "white");}
+	}
 
